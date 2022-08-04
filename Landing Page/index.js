f5233cdad80315c58bb7a75c3612020a1744005e
vaibhav3534 <vaibhavsuryawanshi015@gmail.com>
2022-08-04 17:35:52 +0530

    home page

--- Landing Page/index.js
+++ Landing Page/index.js
@@ -1,4 +1,23 @@
 import navbar from './components/navbar.js';
 
-let navdiv = document.getElementById("navbar_div")
-navdiv.innerHTML = navbar();
+// let navdiv = document.getElementById("navbar_div")
+// navdiv.innerHTML = navbar();
+
+let container = document.getElementById("card_container")
+
+async function getdata(){
+    try {
+        let search = document.getElementById("search").value;
+        let url = `https://api.hotstar.com/s/sniper/forerunner?q=movies&size=1`;
+        let res = await fetch(url);
+        let data = await res.json();
+        console.log(data)
+
+        // return data.results;
+    }
+     catch (error) {
+        console.log("Error in fetching api data")
+    }
+}
+
+getdata();
