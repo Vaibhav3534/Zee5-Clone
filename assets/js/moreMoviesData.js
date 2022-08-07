d4c1cef7c0aee38ddfbad275802075e5eb33f9a3
Akash2377 <surveakash01@gmail.com>
2022-08-07 18:54:18 +0530

    changes done

--- assets/js/moreMoviesData.js
+++ assets/js/moreMoviesData.js
@@ -20,7 +20,10 @@ function ShowMoreResultsInPage(movie) {
   document.getElementById("More-Page-Content-IMG").innerHTML = "";
   movie.map(function (ele, index) {
     let ImgM = `  <img
-          src="https://img1.hotstarext.com/image/upload/f_auto,t_web_hs_1x/${ele.images.h}"
+  
+          src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/${
+            ele.images.v || ele.images.h
+          }"
           alt=""
         />
         <div class="EffectDivForHover">
@@ -49,42 +52,4 @@ function showDivEffect(index) {
 function removediveEffect(index) {
   let hoverEffect = document.querySelectorAll(".EffectDivForHover");
   hoverEffect[index].style.display = "none";
-}
-// ===============================================
-async function getdata() {
-  try {
-    let url = "http://www.omdbapi.com/?apikey=d6f2de94&s=marvel";
-    let res = await fetch(url);
-    let data = await res.json();
-    console.log(data.Search);
-    let card = document.querySelector(".cardbox_container");
-    slidefn1(data.Search);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-function slidefn1(data) {
-  data.forEach(function (ele) {
-    let card = document.querySelector(".cardbox_container1");
-    card.innerHTML =
-      card.innerHTML +
-      `<div class="cardbox">
-            <img src="${ele.Poster}" alt="Mithya" />
-            <div class="crownicon">
-              <a href="#"><i class="fa-solid fa-crown"></i></a>
-            </div>
-            <div class="text_content">
-              <h2>${ele.Title}</h2>
-              <div class="bottom_text">
-                <div class="watch-btn watchbtn">
-                    <a href="">
-                    <i class="fa-solid fa-play " ></i> Watch</
-                     </a> 
-                </div>
-                <p><i class="fa-solid fa-share-nodes"></i> More</p>
-              </div>
-            </div>
-          </div>`;
-  });
 }
